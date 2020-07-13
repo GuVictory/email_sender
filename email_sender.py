56d59c705a81007ef7523dec122bff77479eece5
GuVictory <gubanova.vi10@gmail.com>
2020-07-13 14:20:51 +0500

    fix

--- email_sender.py
+++ email_sender.py
@@ -14,7 +14,7 @@ EMAIL_PASSWORD = getenv("email_password")
 
 email = EmailMessage()
 email['from'] = 'Python Creeeper'
-email['to'] = 'znajderko@yandex.ru'  # znajderko@yandex.ru
+email['to'] = 'email_to_send@gmail.com'
 email['subject'] = 'Super secret msg'
 
 email.set_content(html.substitute(name='Кит кит кашалот'), 'html')
@@ -22,6 +22,7 @@ email.set_content(html.substitute(name='Кит кит кашалот'), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
+    # The login and password from the mail is stored in .env file
     smtp.login(EMAIL_LOGIN, EMAIL_PASSWORD)
     smtp.send_message(email)
     print('Message was send!')
